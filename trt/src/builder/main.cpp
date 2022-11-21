e9d7965ea2d6659ae33e288ca7e2f18e1ba184d0
Yang Fan <yangfan@neolix.cn>
2022-11-21 14:27:10 +0800

    compatible to JP46

--- trt/src/builder/main.cpp
+++ trt/src/builder/main.cpp
@@ -17,6 +17,7 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 */
 #include "builder/builder.h"
+#include <cuda_runtime_api.h>
 #include <getopt.h>
 #include <cstdlib>
 #include <cassert>
